a14a8009147fc24681549a444e06c372b8c91ee5
lazypupz++ <antonycasey08@gmail.com>
2026-01-03 18:53:55 +0000

    changed init

--- init.lua
+++ init.lua
@@ -1,16 +1,66 @@
-print("Hello World")
-require("config.lazy")
+print("Hello World") --required
 
 vim.opt.shiftwidth = 4
 vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
-
---vim.cmd [[hi @function.builtin.lua guifg=red]]
+vim.opt.wrap = false
+vim.opt.tabstop = 4
+vim.opt.swapfile = false
+vim.opt.signcolumn = "yes"
+vim.opt.winborder = "rounded"
 
 vim.g.mapleader = " "
 local map = vim.keymap.set
+
 map('n', '<leader>pv', vim.cmd.Ex)
-vim.lsp.enable({
-    "lua_ls", "clangd", "ruff", "gopls"
+map('n', '<leader>o', ':update<CR> :source<CR>')
+
+vim.pack.add({
+    {src = "https://github.com/neovim/nvim-lspconfig"},
+	{src = "https://github.com/nvim-telescope/telescope.nvim"}
 })
+
+require("config.plugins.telescope")
+
+vim.lsp.config['lua_ls'] = {
+	cmd = { 'lua-language-server' },
+	filetypes = { 'lua' },
+	root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'LuaJIT',
+			}
+		}
+	}
+}
+vim.lsp.config['clangd'] = {
+
+	cmd = { 'clangd'},
+
+	rootmarkers = { '.clangd', 'compile_commands.json'},
+
+	filestypes = {'c', 'cpp'}
+}
+
+vim.lsp.enable({"lua_ls", "clangd"})
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.cmd("colorscheme darkblue")
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('my.lsp', {}),
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		if client:supports_method('textDocument/completion') then
+			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
+			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+			client.server_capabilities.completionProvider.triggerCharacters = chars
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		end
+	end,
+})
+
+vim.cmd [[set completeopt+=menuone,noselect,popup]]
